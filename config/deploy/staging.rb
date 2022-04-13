ba684fd53b05491add3aa7c4dd2375212260aa4f
Sneha Aradhey <aradheysv@gmail.com>
2022-04-13 17:29:43 -0400

    dummy_trigger_16

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server 'lin-res44.csc.ncsu.edu', user: 'svaradhe', roles: %w{web app}, my_property: :my_value
+server 'lin-res44.csc.ncsu.edu', user: 'svaradhe', roles: %w{web app db}, my_property: :my_value
 
 # role-based syntax
 # ==================
@@ -41,7 +41,7 @@ server 'lin-res44.csc.ncsu.edu', user: 'svaradhe', roles: %w{web app}, my_proper
 # Global options
 # --------------
 #  set :ssh_options, {
-#    keys: %w(/Users/adwaittathe/.ssh/id_rsa),
+#    keys: %w(/Users/.ssh/id_rsa),
 #    forward_agent: false,
 #    auth_methods: %w(password)
 #  }
